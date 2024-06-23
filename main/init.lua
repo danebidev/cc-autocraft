ef2b078f3a9731ced238dbcc89d7e46d94d1b06a
danebidev <57720273+danebidev@users.noreply.github.com>
2024-06-23 21:07:49 +0200

    Refactor

--- main/init.lua
+++ main/init.lua
@@ -1,3 +1,4 @@
+-- Setup {{{
 local monitor = peripheral.find("monitor") --[[@as ccTweaked.peripherals.Monitor]]
 
 if not monitor then
@@ -29,23 +30,10 @@ local function printToMonitor(text)
         monitor.write(line)
     end
 end
+printToMonitor("[ ] Loading main.lua")
+-- }}}
 
-local function deleteLastLine()
-    if not monitor then
-        return
-    end
-
-    table.remove(lines, #lines)
-    monitor.clear()
-    monitor.setCursorPos(1, 1)
-    for i, line in ipairs(lines) do
-        monitor.setCursorPos(1, i)
-        monitor.write(line)
-    end
-end
-
-printToMonitor("[x] Loading main.lua")
-
+-- Recipe handlers {{{
 printToMonitor("[ ] Finding recipe handlers")
 
 local handlers = {}
@@ -57,29 +45,43 @@ for _, file in ipairs(files) do
     local name = file:match("(.+).lua")
     if name then
         num_handlers = num_handlers + 1
-        printToMonitor("[x] Found handler " .. name)
+        printToMonitor("    Found handler \"" .. name .. "\"")
         handlers[name] = dofile(fs.combine(root, "recipe_handlers", file))
     end
 end
 
 printToMonitor("[x] Found " .. num_handlers .. " recipe handlers")
+-- }}}
 
-printToMonitor("[ ] Finding drives with recipes")
+-- Drives {{{
+local function findDrivesWithRecipes()
+    printToMonitor("[ ] Finding drives with recipes")
 
-local num_drives = 0
+    local num_drives = 0
 
-local drives = peripheral.find("drive", function(_, object)
-    if not object.hasData() then
-        return false
+    local drives = peripheral.find("drive", function(name, drive)
+        if not drive.hasData() or not fs.exists(fs.combine(name, "recipes")) then
+            return false
+        end
+
+        printToMonitor("    Found drive " .. name .. " with recipes")
+        num_drives = num_drives + 1
+        return true
+    end)
+
+
+    if not drives then
+        printToMonitor("[x] No drives found with recipes")
+        return {}
+    else
+        printToMonitor("[x] Found " .. num_drives .. " drives with recipes")
+        return drives
     end
-    num_drives = num_drives + 1
-    return true
-end)
-
-deleteLastLine()
-
-if not drives then
-    printToMonitor("[x] No drives found with recipes")
-else
-    printToMonitor("[x] Found " .. num_drives .. " drives with recipes")
 end
+-- }}}
+
+local drives = findDrivesWithRecipes()
+
+-- Main loop {{{
+
+-- }}}
