088f417076134ad286dfc7907172867dce9dfa6c
danebidev <57720273+danebidev@users.noreply.github.com>
2024-06-23 17:16:59 +0200

    Started initialization

--- main.lua
+++ main.lua
@@ -1,10 +1,40 @@
-local monitor = peripheral.find("monitor")
+local monitor = peripheral.find("monitor") --[[@as ccTweaked.peripherals.Monitor]]
 
-if monitor then
-    monitor.setTextScale(0.5)
-    monitor.clear()
-    monitor.setCursorPos(1, 1)
-    monitor.write("Hello, World!")
-else
+if not monitor then
     print("No monitor found")
 end
+
+monitor.clear()
+monitor.setCursorPos(1, 1)
+
+local lines = {}
+
+local function printToMonitor(text)
+    table.insert(lines, text)
+
+    if #lines > monitor.getSize() - 1 then
+        table.remove(lines, 1)
+    end
+
+    monitor.clear()
+    monitor.setCursorPos(1, 1)
+    for i, line in ipairs(lines) do
+        monitor.setCursorPos(1, i)
+        monitor.write(line)
+    end
+end
+
+printToMonitor("[x] Loading main.lua")
+
+printToMonitor("[ ] Finding drives with recipes")
+
+local drives = peripheral.find("drive", function(name, object)
+    return object.hasData()
+end)
+
+if not drives then
+    printToMonitor("[x] No drives found with recipes")
+    return
+end
+
+printToMonitor("[x] Found " .. #drives .. " drives with recipes")
