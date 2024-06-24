156dddb63315ea79c6df5c176879c977d794865b
danebidev <57720273+danebidev@users.noreply.github.com>
2024-06-24 16:52:52 +0200

    .

--- user_api.lua
+++ user_api.lua
@@ -1,24 +1,24 @@
 local lib = require("lib")
 
 local function run()
-	lib.printToMonitor("[x] Started user API")
-	peripheral.find("modem", rednet.open)
+    lib.printToMonitor("[x] Started user API")
+    peripheral.find("modem", rednet.open)
 
-	rednet.host("autocrafting", "main")
+    rednet.host("autocrafting", "main")
 
-	while true do
-		local sender, message = rednet.receive("autocrafting")
+    while true do
+        local sender, message = rednet.receive("autocrafting")
 
-		local command = lib.split(message, " ")
+        local command = lib.split(message, " ")
 
-		if command[1] == "get_raw" then
-			if not Data.recipes[command[2]] then
-				rednet.send(sender, "Recipe not found")
-			else
-				rednet.send(sender, textutils.serialize(Data.recipes[command[2]]))
-			end
-		end
-	end
+        if command[1] == "get_raw" then
+            if not Data.recipes[command[2]] then
+                rednet.send(sender, "Recipe not found", "autocrafting")
+            else
+                rednet.send(sender, textutils.serialize(Data.recipes[command[2]]), "autocrafting")
+            end
+        end
+    end
 end
 
 return { run = run }
